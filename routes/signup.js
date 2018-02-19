1cbadde429fd742c79f162c33933a6ad74e78d64
Aakash Kumar Goryan <aakashgoryan@gmail.com>
2018-02-19 14:07:52 +0530

    added express validator

---
+++ routes/signup.js
@@ -0,0 +1,17 @@
+let express = require('express');
+let router = express.Router();
+
+/* GET users listing. */
+router.post('/', function(req, res, next) {
+    req.check('Email','Invalid email address').isEmail();
+    req.check('Password','Password is invalid').isLength({min: 8}).equals(req.body.ConfirmPassword);
+    req.check('DateOfBirth','Date of birth invalid').isISO8601();
+    req.check('Country','Not a country').isISO31661Alpha2();
+    let errors = req.validationErrors();
+    if(errors) {
+        res.redirect('/');
+        res.send(errors);
+    }
+});
+
+module.exports = router;
