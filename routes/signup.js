9c473c4ccf1710bb0956d4a69a162159ed068290
Aakash Kumar Goryan <aakashgoryan@gmail.com>
2018-02-19 17:02:40 +0530

    Home.html will open on localhost:8080

--- routes/signup.js
+++ routes/signup.js
@@ -7,15 +7,19 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
+    console.log(req.body)
     req.check('Email','Invalid email address').isEmail();
     req.check('Password','Password is invalid').isLength({min: 8}).equals(req.body.ConfirmPassword);
     req.check('DateOfBirth','Date of birth invalid').isISO8601();
     req.check('Country','Not a country').isISO31661Alpha2();
     let errors = req.validationErrors();
     if(errors) {
-        res.redirect('/');
-        res.send(errors);
+        console.log(errors);
+        res.send('errores');
+        //res.send(errors);
     }
+    else
+        res.send("done");
 });
 
 module.exports = router;
